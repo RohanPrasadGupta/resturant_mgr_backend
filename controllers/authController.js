c9119b2b34a79dcddb113d5298130c7669e9459c
Rohan <rohanprasadgupta4@gmail.com>
2025-06-24 16:55:14 +0700

    remove console data

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,7 +14,6 @@ exports.authCheck = async (req, res, next) => {
     const decoded = jwt.decode(token);
 
     const user = await User.findById(decoded.id);
-    console.log("User found:", user);
     if (!user) {
       return res.status(401).json({
         message: "Invalid token. User not found.",
