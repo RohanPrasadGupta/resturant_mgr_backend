26e93ba518bec54f7809b2adf6284b327ae0240b
Rohan <rohanprasadgupta4@gmail.com>
2025-06-27 15:33:22 +0700

    update control

--- controllers/userController.js
+++ controllers/userController.js
@@ -65,7 +65,7 @@ exports.login = async (req, res) => {
     res.cookie("mgr-token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "production" ? "none" : "lax",
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
 
